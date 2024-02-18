965114f43c7cb838ee84ac53e84d344d6191fa86
sirajul116 <hridoyhrix116@gmail.com>
2024-02-19 02:45:51 +0600

    click arrow to go bottom of the page

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var a = document.querySelector('#contct');
 var b = document.querySelector('.popup');
 var c = document.querySelector('.close');
+var d = document.querySelector('#cls');
+var e = document.querySelector('.arrow');
 
 a.addEventListener('click', function () {
   b.style.display = 'flex';
@@ -8,4 +10,12 @@ a.addEventListener('click', function () {
 
 c.addEventListener('click', function () {
   b.style.display = 'none';
+});
+
+d.addEventListener('click', function () {
+  b.style.display = 'none';
+});
+
+e.addEventListener('click', function () {
+  document.body.scrollIntoView({ behavior: 'smooth', block: 'end' });
 });

97c441b5fc4e8d6402e94ea5d7da8bff52897b44
sirajul116 <hridoyhrix116@gmail.com>
2024-03-08 02:26:44 +0600

    Implment React and Tailwind css and build the Navbar

---
+++ PortfolioReact/src/components/Navbar.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <div className="h-20 border flex justify-between items-center px-16">
+      <div className="">
+        <img src="./src/assets/logo.png" className="h-16" />
+      </div>
+      <div className="space-x-9">
+        <a href="/about" className="hover:text-orange-600">
+          about
+        </a>
+        <a href="/experience" className="hover:text-orange-600">
+          experience
+        </a>
+        <a href="/project" className="hover:text-orange-600">
+          project
+        </a>
+        <a href="/contact" className="hover:text-orange-600">
+          contact
+        </a>
+      </div>
+      <div className="space-x-3">
+        <button className="px-3 py-2 border rounded-md hover:text-orange-600">
+          Resume
+        </button>
+        <button className="px-3 py-2 border rounded-md hover:text-orange-600">
+          Hire Me
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;

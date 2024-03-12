964c45e4df19fb75e50f8f9337155954bad5f52f
sirajul116 <hridoyhrix116@gmail.com>
2024-03-12 22:07:11 +0600

    About section updated with horizontal line

--- PortfolioReact/src/components/Home.jsx
+++ PortfolioReact/src/components/Home.jsx
@@ -4,10 +4,11 @@ function Home() {
   return (
     <div className="w-full h-screen border grid place-items-center">
       <div>
-        <h1 className="text-3xl"> Hi, I'm Hridoy</h1>
-        <p>I'm a Learner and Enthusiast</p>
-
-        <img src="./src/assets/hero-arrow.png" />
+        <h1 className="text-3xl grid place-items-center"> Hi, I'm Hridoy</h1>
+        <p className="grid place-items-center">I'm a Learner and Enthusiast</p>
+        <div class="flex justify-center items-center scroll-smooth cursor-pointer">
+          <img src="./src/assets/hero-arrow.png" />
+        </div>
       </div>
     </div>
   );
